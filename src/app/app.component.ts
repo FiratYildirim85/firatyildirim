03f41aa42f1e53fff5d61a47b21934d501b6192b
FiratYildirim85 <firat.yildirim2904@gmail.com>
2022-11-28 22:41:11 +0100

    Added images and startet to work on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'firatyil';
+  images = ['beach.jpg', 'girl.jpg', 'maldives.jpg'];
+  currentImage = 0
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() =>{
+      this.currentImage++;                                        //images ist nach 8 sekunden nicht 0 sondern 1
+      this.currentImage = this.currentImage % this.images.length  // 3/3  Rest 0
+      this.showImage = false;
+
+      setTimeout(()=>{
+        this.showImage = true;
+      },10)
+    
+    }, 8000);
+  }
+
 }
