8f74bb36c279aa52f305aef0e64376b6f388c8c8
FiratYildirim85 <firat.yildirim2904@gmail.com>
2022-11-29 10:58:07 +0100

    started menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,28 +5,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'firatyil';
-  images = ['beach.jpg', 'girl.jpg', 'maldives.jpg'];
-  currentImage = 0
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-
-    setInterval(() =>{
-      this.currentImage++;                                        //images ist nach 8 sekunden nicht 0 sondern 1
-      this.currentImage = this.currentImage % this.images.length  // 3/3  Rest 0
-      this.showImage = false;
-
-      setTimeout(()=>{
-        this.showImage = true;
-      },10)
-    
-    }, 8000);
-  }
-
+export class AppComponent {
 }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+
+
+
+  title = 'firatyil';
+  images = ['software-developer.jpg', 'computer.jpg', 'coding.jpg'];
+  headlines = ['Bring Holiday to the next Level', 'Dreamland for you', 'maldives waiting']
+  currentImage = 0
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;                                        //images ist nach 8 sekunden nicht 0 sondern 1
+      this.currentImage = this.currentImage % this.images.length  // 3/3  Rest 0
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10)
+
+    }, 8000);
+  }
+}
